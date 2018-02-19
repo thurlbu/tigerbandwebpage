954976c13028c950784921519903d62df1d95c2b
BurleyHurly <thurlbu@clemson.edu>
2018-02-19 11:31:23 -0500

    Content Update
    Updated article content to homepage

--- website/index.php
+++ website/index.php
@@ -11,18 +11,18 @@
 </head>
 
 <!--- HEADER --->
-    
-<input type='checkbox' id='mobileMenu'>	
+
+<input type='checkbox' id='mobileMenu'>
 
 <div id="mobileHeader">
 	<a href="/website/"><img id="tbMobileLogo" src="/img/logos/TigerBandMobileLogo.png" width="120"></a>
 	<label for="mobileMenu" id="menuButton"><span>=</span></label>
 </div>
-	
+
 <label id='mobileScreenShade' for='mobileMenu'></label>
-	
+
 <!--- NAVIGATION --->
-    
+
 <nav class="homepage">
     <a href="/website/" class="active">Home</a>
     <a href="/this_season/">This Season</a>
@@ -32,31 +32,31 @@
     <a href="/about/">About</a>
     <a href="/members/">Members Only</a>
     <a href="/contact/">Contact</a>
-</nav> 
-    
+</nav>
+
 <!--- PAGE CONTENT --->
-    
+
 <body>
     <!--- FULL SCREEN MAIN VIDEO --->
-    
+
     <div id="homeFullscrContainer">
         <div id="screenShadeHome"></div>
         <video id="homeVideoBkg" autoplay loop muted>
             <source src="/img/WebsiteVideo.mp4" type="video/mp4">
         </video>
     </div>
-    
+
     <div id="homepageTitle">
         <h2>Clemson University</h2>
         <h1>Tiger Band</h1>
     </div>
-    
-    
+
+
     <img id="tbHomeLogo" src="/img/logos/TigerBandWhite.png">
     <img id="clemsonLogo" src="/img/logos/clemsonPaw.svg" width="75">
-    
+
     <!--- BOX UI --->
-    
+
     <div id="boxUI">
         <a href="http://www.tigerbandmedia.com/">
             <div class="boxWrapper">
@@ -78,11 +78,11 @@
                 <div id="box3" class="boxUIelement sq white homepage">
                     <div class="textContainer hide">
                         <p>Clemson University<br/>Tiger Band<br/>YouTube</p>
-                    </div>    
+                    </div>
                 </div>
             </div>
         </a>
-    
+
         <a href="https://cualumni.clemson.edu/cutba-interest" target="_blank" class="boxWrapper">
             <div id="box4" class="boxUIelement sq purp homepage">
                 <div class="textContainer hide">
@@ -138,7 +138,7 @@
                 </div>
             </div>
         </a>
-        
+
         <a href="http://www.clemsontigerbandgear.com/" target="_blank" class="boxWrapper">
             <div id="box9" class="boxUIelement sq purp homepage">
                 <div class="textContainer hide">
@@ -147,25 +147,25 @@
             </div>
         </a>
     </div>
-    
+
     <!--- ANNOUNCEMENTS/ARTICLES --->
-    
+
     <div id="headingOrange">
         <h3 class="orangeText">Tiger Band</h3>
         <h4 class="orangeText">Announcements</h4>
     </div>
-    
+
     <div id="Col2FlexContent" class="homepage">
         <div id="col1Flex">
             <article class="text">
                 <h1>Join the 2018 Clemson University Tiger Band</h1>
                 <a href="/join/" class="boxWrapper"><img class="articleImg" src='/img/Basco.jpg'></a>
-                <p>Do you have what it takes to be a "Guardian of the Clemson Spirit?"</p> 
+                <p>Do you have what it takes to be a "Guardian of the Clemson Spirit?"</p>
             <p>For information on audition materials, deadlines, and answers to your questions about what it's like to be a member of the "Band that Shakes the Southland," check out our <a href="/join/">join page.</a></p>
             <p>The only thing missing from Tiger Band's 2018 season is YOU!</p>
             <p>Submit your audition video today!</p>
             </article>
-            
+
             <article class="media">
                 <h1>Lee Greenwood performs with Tiger Band</h1>
                 <p>Tiger Band and Lee Greenwood perform God Bless the USA on Military Appreciation Day!</p>
@@ -175,7 +175,7 @@
                     </div>
                 </div>
             </article>
-            
+
             <article class="media">
                 <h1>We are Tiger Band</h1>
                 <p>What does it mean to be a member of Tiger Band?</p>
@@ -186,18 +186,26 @@
                 </div>
             </article>
         </div>
-        
+
         <div id="col2Flex">
+          <article class="text">
+              <h1>Tiger Band Tours</h1>
+              <p>If you are planning a campus visit, don't forget to sign up for a Tiger Band Tour to learn first-hand what it's like to be a member of the "Band that Shakes the Southland!"</p>
+              <p>Tour our facilities, learn about the history and traditions of the band, and experience a gameday through the eyes of our Tiger Band Ambassadors.</p>
+              <p>Register for a free tour today!</p>
+              <a class="richLink cta" href="https://www.google.com/forms/about/" target="_blank">Sign-up</a>
+          </article>
+
             <article class="media">
                 <h1>Tiger Band at the 2018 Sugar Bowl</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut magna tortor.</p>
-                <div class="iframeContainer">    
+                <p>Tiger Band performs "The Spy who Saved Me" at the 2018 Sugar Bowl.</p>
+                <div class="iframeContainer">
                     <div class="iFrameAspectRatio">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/sIAt07BUOoo" frameborder="0" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/oMHpLtQUejM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                     </div>
                 </div>
             </article>
-            
+
             <article class="text">
                 <h1>Tiger Band meets John F Kennedy</h1>
                     <a href="https://clemson.world/tiger-band-jfk/" class="boxWrapper"><img class="articleImg" src='/img/Kennedy.jpg'></a>
@@ -205,21 +213,26 @@
                 <p>Read about this meeting in the <a href="https://clemson.world/tiger-band-jfk/">wonderful article</a> in the Clemson World Magazine written by former director of Tiger Band, John Butler.</p>
             </article>
             
-            <article class="text">
-                <h1>Tiger Band Tours</h1>
-                <p>What does it mean to be a member of Tiger Band?</p>
-                <a class="richLink cta" href="https://www.google.com/forms/about/" target="_blank">Sign-up</a>
+                        <article class="media">
+                <h1>#TigerBandTour Vlog, Episode 1: The Sugar Bowl</h1>
+                <p>Relive our trip to New Orleans for the 2018 Sugar Bowl through the eyes of a few current Tiger Band students!</p>
+                <div class="iframeContainer">
+                    <div class="iFrameAspectRatio">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/CbF_BlUeyWQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    </div>
+                </div>
             </article>
+
         </div>
     </div>
 </body>
-    
+
 <!--- FOOTER --->
 
 <?php
     readfile("../footer.html");
 ?>
-    
+
 <script type="text/javascript">
     //Copyright Date
     var startYear = 2017;
