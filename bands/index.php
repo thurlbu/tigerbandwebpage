cfbc6ef8aa8ab822e0d69da765c25a87b98ddf2a
Adam Patyk <apatyk@g.clemson.edu>
2018-02-27 19:18:21 -0500

    Updated calendar design and implemented clickable articles

--- bands/index.php
+++ bands/index.php
@@ -44,67 +44,82 @@
 
     <div id="Col2FlexContent">
         <div id="col1Flex">
-            <article class="text">
-                <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/symphonic.html"><h1>Symphonic Band</h1>
-                    <img class="articleImg" src='/img/SymphonicBand.jpg'></a>
+            <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/symphonic.html">
+              <article class="text">
+                <h1>Symphonic Band</h1>
+                    <img class="articleImg" src='/img/SymphonicBand.jpg'>
                 <p>The Clemson University Symphonic Band is a select wind ensemble that performs contemporary and traditional concert band literature.</p>
                 <p>Frequent campus concert and tour appearances have earned high national praise for this band. Previous tours have included performances at the 2012 London Olympics, Carnegie Hall, Lincoln Center, The SCMEA Conference in Columbia, SC, New Orleans' Jackson Square, and Walt Disney World.</p>
-            </article>
+              </article>
+            </a>
+            <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/jazz.html">
+              <article class="text">
+                  <h1>Jazz Ensemble</h1>
+                      <img class="articleImg" src='/img/JazzEnsemble.jpg'>
+                  <p>Clemson University's Jazz Ensemble, also know as "The Jungaleers," (MUSIC 3630) is a Big Band with standard instrumentation.</p>
+                  <p>The ensemble performs music of many different genres and has a personality and flair all its own. If you love to have fun playing music, consider joining us!</p>
+              </article>
+            </a>
 
-            <article class="text">
-                <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/jazz.html"><h1>Jazz Ensemble</h1>
-                    <img class="articleImg" src='/img/JazzEnsemble.jpg'></a>
-                <p>Clemson University's Jazz Ensemble, also know as "The Jungaleers," (MUSIC 3630) is a Big Band with standard instrumentation.</p>
-                <p>The ensemble performs music of many different genres and has a personality and flair all its own. If you love to have fun playing music, consider joining us!</p>
-            </article>
+            <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/pep.html">
+              <article class="text">
+                  <h1>Basketball Pep Band</h1>
+                  <img class="articleImg" src='/img/clemson-tiger-band-louisville-2016-345.jpg'>
+                  <p>A vital element in promoting Tiger Spirit, the Tiger Pep Bands perform at men's and women's home basketball games and other University functions. The Pep Bands also travels to the Atlantic Coast Conference and NCAA basketball tournaments.</p>
+              </article>
+            </a>
 
-            <article class="text">
-                <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/pep.html"><h1>Basketball Pep Band</h1>
-                    <img class="articleImg" src='/img/clemson-tiger-band-louisville-2016-345.jpg'></a>
-                <p>A vital element in promoting Tiger Spirit, the Tiger Pep Bands perform at men's and women's home basketball games and other University functions. The Pep Bands also travels to the Atlantic Coast Conference and NCAA basketball tournaments.</p>
-            </article>
-
-            <article class="text">
-                <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/steelband.html"><h1>Percussion Ensemble &amp; Steel Band</h1>
-                    <img class="articleImg" src='/img/clemson-tiger-band-louisville-2016-345.jpg'></a>
-                <p>The Steel Band includes 18 players and features 4 lead pans, 2 double seconds, 2 cellos, one bass, and engine room. The band performs a variety of musical styles including calypso, soca, pop, classical, and Latin.</p>
-                <p>The Gigging Steel Band, founded in 2003, is a select group of 6 players and is a professional ensemble available for hire. The Gigging Steel Band is active in the fall and spring semesters and performs on campus and in the community.</p>
-                <p>The Clemson Steel Band has recorded 3 CDs: Steelin' the Spotlight, One Small Step for Pan, and Pan Overboard.</p>
-                <p>The Clemson Steel Band performed with steelpan virtuoso Liam Teague in April 2016 and has also performed with Tom Miller, Chris Wabich, and Arthur Lipner among others. </p>
-            </article>
+            <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/steelband.html">
+              <article class="text">
+                  <h1>Percussion Ensemble &amp; Steel Band</h1>
+                    <img class="articleImg" src='/img/clemson-tiger-band-louisville-2016-345.jpg'>
+                  <p>The Steel Band includes 18 players and features 4 lead pans, 2 double seconds, 2 cellos, one bass, and engine room. The band performs a variety of musical styles including calypso, soca, pop, classical, and Latin.</p>
+                  <p>The Gigging Steel Band, founded in 2003, is a select group of 6 players and is a professional ensemble available for hire. The Gigging Steel Band is active in the fall and spring semesters and performs on campus and in the community.</p>
+                  <p>The Clemson Steel Band has recorded 3 CDs: Steelin' the Spotlight, One Small Step for Pan, and Pan Overboard.</p>
+                  <p>The Clemson Steel Band performed with steelpan virtuoso Liam Teague in April 2016 and has also performed with Tom Miller, Chris Wabich, and Arthur Lipner among others. </p>
+              </article>
+            </a>
         </div>
 
         <div id="col2Flex">
-            <article class="text">
-                <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/concert.html"><h1>Concert Band</h1>
-                    <img class="articleImg" src='/img/ConcertBand.jpg'></a>
-                <p>The Clemson University Concert Band is open to all University students and community members without audition. The band rehearses once a week during the spring semester and presents a popular concert in April.</p>
-            </article>
+            <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/concert.html">
+              <article class="text">
+                  <h1>Concert Band</h1>
+                      <img class="articleImg" src='/img/ConcertBand.jpg'>
+                  <p>The Clemson University Concert Band is open to all University students and community members without audition. The band rehearses once a week during the spring semester and presents a popular concert in April.</p>
+              </article>
+            </a>
 
-            <article class="text">
-                <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/jazzcombo.html"><h1>Jazz Combos</h1>
-                    <img class="articleImg" src='/img/clemson-tiger-band-louisville-2016-345.jpg'></a>
-                <p>The Clemson Jazz Combo is a performance course designed to introduce students to small group literature from Be-Bop to the present.</p>
-                <p>Generally consisting of four to eight members, the ensemble emphasizes improvisation, group interaction, and includes solo transcription and analysis.</p>
-                <p>Special attention is given jazz theory and the application of scalar and modal concepts, harmonic function and substitution, rhythmic development, music reading skills and technical proficiency.</p>
-            </article>
+            <a href="http://www.clemson.edu/caah/departments/performing-arts/ensembles/Music/Instrumental/Band/jazzcombo.html">
+              <article class="text">
+                  <h1>Jazz Combos</h1>
+                      <img class="articleImg" src='/img/clemson-tiger-band-louisville-2016-345.jpg'>
+                  <p>The Clemson Jazz Combo is a performance course designed to introduce students to small group literature from Be-Bop to the present.</p>
+                  <p>Generally consisting of four to eight members, the ensemble emphasizes improvisation, group interaction, and includes solo transcription and analysis.</p>
+                  <p>Special attention is given jazz theory and the application of scalar and modal concepts, harmonic function and substitution, rhythmic development, music reading skills and technical proficiency.</p>
+              </article>
+            </a>
 
-            <article class="text">
-                <a href="http://tband.people.clemson.edu/contact/"><h1>Tiger Town Brass</h1>
-                    <img class="articleImg" src='/img/clemson-tiger-band-louisville-2016-345.jpg'></a>
-                <p>Interested in adding some Clemson Spirit to your event?</p>
-                <p>Tiger Town Brass is made up of 10-members of Tiger Band and is available to hire to play your favorite Clemson songs at your event.</p>
-                <p>Tiger Town Brass also performs at select Clemson Volleyball matches and University events.</p>
-                <p>For information on hiring Tiger Town Brass, please contact the Band Office through the contact form or by calling 864.656.3380.</p>
-            </article>
+            <a href="http://tband.people.clemson.edu/contact/">
+              <article class="text">
+                  <h1>Tiger Town Brass</h1>
+                      <img class="articleImg" src='/img/clemson-tiger-band-louisville-2016-345.jpg'>
+                  <p>Interested in adding some Clemson Spirit to your event?</p>
+                  <p>Tiger Town Brass is made up of 10-members of Tiger Band and is available to hire to play your favorite Clemson songs at your event.</p>
+                  <p>Tiger Town Brass also performs at select Clemson Volleyball matches and University events.</p>
+                  <p>For information on hiring Tiger Town Brass, please contact the Band Office through the contact form or by calling 864.656.3380.</p>
+              </article>
+            </a>
 
-            <article class="text">
-                <a href="http://tband.people.clemson.edu/drumline/"><h1>Clemson University Drumline (CUD)</h1>
-                    <img class="articleImg" src='/img/clemson-tiger-band-fiesta-bowl-2016-726.jpg'></a>
-                <p>The Clemson University Drumline (CUD) is Clemson's premier marching percussion ensemble  and is a section of the Clemson University Tiger Band - known as "The Band That Shakes The Southland." Being part of the drumline at Clemson not only teaches students how to have confidence and perform to an audience, it also teaches value of teamwork, provides character development, and builds leadership skills. Rich tradition, high standards, and performance excellence are trademarks of both the Clemson University Drumline and Tiger Band.</p>
-                <p>The mission of the Clemson Drumline is to exhibit the core values that define CUD, to demonstrate excellence in everything we do, and to improve from rehearsal to rehearsal, game to game, year to year.</p>
-                <p>The Clemson Drumline is sponsored by Yamaha Marching Percussion, Sabian Cymbals, Remo Drumheads, and Vic Firth Drumsticks. We greatly value the relationships we have with all of our sponsors.</p>
-            </article>
+            <a href="http://tband.people.clemson.edu/drumline/">
+              <article class="text">
+                  <h1>Clemson University Drumline (CUD)</h1>
+                      <img class="articleImg" src='/img/clemson-tiger-band-fiesta-bowl-2016-726.jpg'>
+                  <p>The Clemson University Drumline (CUD) is Clemson's premier marching percussion ensemble  and is a section of the Clemson University Tiger Band - known as "The Band That Shakes The Southland." Being part of the drumline at Clemson not only teaches students how to have confidence and perform to an audience, it also teaches value of teamwork, provides character development, and builds leadership skills. Rich tradition, high standards, and performance excellence are trademarks of both the Clemson University Drumline and Tiger Band.</p>
+                  <p>The mission of the Clemson Drumline is to exhibit the core values that define CUD, to demonstrate excellence in everything we do, and to improve from rehearsal to rehearsal, game to game, year to year.</p>
+                  <p>The Clemson Drumline is sponsored by Yamaha Marching Percussion, Sabian Cymbals, Remo Drumheads, and Vic Firth Drumsticks. We greatly value the relationships we have with all of our sponsors.</p>
+              </article>
+            </a>
         </div>
     </div>
 
