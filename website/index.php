a0c7bae10c99938913c5201aaa0d8e9303e68a88
Nicholas Gustafson <nichgus7@gmail.com>
2018-03-28 19:09:54 -0400

    Updated link for video, larger video

--- website/index.php
+++ website/index.php
@@ -41,7 +41,7 @@
     <div id="homeFullscrContainer">
         <div id="screenShadeHome"></div>
         <video id="homeVideoBkg" autoplay loop muted>
-            <source src="https://s3.amazonaws.com/tiger-band-website/WebsiteVideo.mp4" type="video/mp4">
+            <source src="https://s3.amazonaws.com/tiger-band-website/WebPageVideoLarge.mp4" type="video/mp4">
         </video>
     </div>
 
