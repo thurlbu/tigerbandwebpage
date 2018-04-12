53429ab75643a66772eeb4a43e83b973e3a6a4b4
Adam Patyk <apatyk@g.clemson.edu>
2018-04-12 10:22:22 -0400

    Added Drumline and CUTBA subdirectories
    Added the Drumline and CUTBA pages and all subdirectories to this Git

---
+++ drumline/contact/contact.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Contact | Clemson Drumline</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="The official website of the Clemson University Drumline">
+    <meta name="keywords" content="Clemson, University, Drumline, CUD, Marching, Percussion, Band, Drum Line, Yamaha, Drums, Remo, Drumheads, Heads, Sabian, Cymbals, Vic Firth, Drumsticks, Sticks, Sheet Music, Music, mp3, pdf, Technique, Manuals, Articles, Audition, Schedule, Information, Packets, Packet, Photo, Gallery, Members, Instructional, Staff">
+    <meta name="copyright" content="2009-2017 Clemson University Drumline">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f66733">
+    <meta name="theme-color" content="#522d80">
+</head>
+
+<input type='checkbox' id='mobileMenu'>	
+
+<!--- HEADER --->    
+
+<div id="mobileHeader">
+	<a href="/drumline/"><img id="tbMobileLogo" src="../img/logos/Drumline%20Mobile%20Header.svg" width="120"></a>
+	<label for="mobileMenu" id="menuButton"><span>=</span></label>
+</div>
+	
+<label id='mobileScreenShade' for='mobileMenu'></label>  
+    
+<!--- NAVIGATION --->
+    
+<nav>
+    <a href="../">Home</a>
+    <a href="../about">About</a>
+    <a href="../auditions/">Auditions</a>
+    <a href="../music/">Music</a>
+    <a href="../contact/" class="active">Contact</a>
+</nav>    
+    
+<!--- PAGE CONTENT --->
+    
+<body class="stickyFooter">
+    <div class="stickyFooterContent">
+        <div id="heading">
+            <h3>Contact</h3>
+            <h4>Clemson University Drumline</h4>
+        </div>
+
+        <center>
+            <h4>Thank you for your message!</h4>
+            <br>
+            <h3><a href="/drumline/">Return to the homepage</a></h3>
+        </center>
+    </div>
+    
+<!--- FOOTER --->
+    
+    <footer>
+        <div id="footerColsLayout">
+            <table>
+                <tr>
+                    <td>NAVIGATION</td>
+                </tr>
+                <tr>
+                    <td><a href="about/">About</a></td>
+                </tr>
+                <tr>
+                    <td><a href="auditions/">Auditions</a></td>
+                </tr>
+                <tr>
+                    <td><a href="music/">Music</a></td>
+                </tr>
+
+                <tr>
+                    <td><a href="contact/">Contact</a></td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <td>AUDITION FOR TIGER BAND</td>
+                </tr>
+                <tr>
+                    <td><a href="/join/winds/">Winds</a></td>
+                </tr>
+                <tr>
+                    <td><a href="http://drum.people.clemson.edu/auditions/">Drumline</a></td>
+                </tr>
+                <tr>
+                    <td><a href="/join/guard/">Color Guard</a></td>
+                </tr>
+                <tr>
+                    <td><a href="/join/dancers/">Dancers</a></td>
+                </tr>
+                <tr>
+                    <td><a href="/join/twirlers/">Twirlers</a></td>
+                </tr>
+            </table>
+            <table>
+                <td>
+                    <a href="/drumline/"><img id="footerLogo" src="../img/logos/TigerBandFull.png"></a>
+                </td>
+            </table>
+            <table>
+                <tr>
+                    <td>DEVELOPERS</td>
+                </tr>
+                <tr>
+                    <td><a href="https://www.linkedin.com/in/alyssa-collado" target="_blank">Alyssa Collado</a></td>
+                </tr>
+                <tr>
+                    <td><a href="https://www.linkedin.com/in/nicholas-gustafson/" target="_blank">Nicholas Gustafson</a></td>
+                </tr>
+                <tr>
+                    <td><a href="http://adampatyk.io" target="_blank">Adam Patyk</a></td>
+                </tr>
+            </table>
+             <table>
+                <tr>
+                    <td id="copyright">
+                        <noscript>
+                            COPYRIGHT &copy; 2017
+                        </noscript>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Clemson University</td>
+                </tr>
+                <tr>
+                    <td>Tiger Band</td>
+                </tr>
+                <tr>
+                    <td> </td>
+                </tr>
+                <tr>
+                    <td><a href="contact/">Contact CUD</a></td>
+                </tr>
+            </table>
+        </div>
+        <div id="line"></div>
+        <div id="socMedContainer">
+            <div id="icons">
+                <a href="https://twitter.com/clemsondrumline" target="_blank">
+                    <div id="twitter" class="socialBorder">
+                        <img class="socialIcon" src="../img/logos/twitter.png">
+                    </div>
+                </a>
+                <a href="https://www.instagram.com/clemson_drumline/" target="_blank">
+                    <div id="instagram" class="socialBorder">
+                        <img class="socialIcon" src="../img/logos/instagram.png">
+                    </div>
+                </a>
+                <a href="https://www.facebook.com/ClemsonDrumline" target="_blank">
+                    <div id="facebook" class="socialBorder">
+                        <img class="socialIcon" src="../img/logos/facebook.png">
+                    </div>
+                </a>
+                <a href="https://www.snapchat.com/add/cutigerband" target="_blank">
+                    <div id="snapchat" class="socialBorder">
+                        <img class="socialIcon" src="../img/logos/snapchat.png">
+                    </div>
+                </a>
+            </div>
+        </div>
+    </footer>
+</body>
+    
+<script type="text/javascript">
+    //Copyright Date
+    var startYear = 2017;
+    var currentYear = new Date().getFullYear();
+    if (startYear == currentYear) {
+        document.getElementById("copyright").innerHTML = "COPYRIGHT &copy " + startYear;
+    }
+    else {
+        document.getElementById("copyright").innerHTML = "COPYRIGHT &copy " + startYear + " - " + currentYear;
+    }
+</script>
+</html>
