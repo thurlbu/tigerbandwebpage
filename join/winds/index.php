7f21c732b273d9a0e57f4f0dd92aa4201ac3d28d
BurleyHurly <thurlbu@clemson.edu>
2018-04-11 20:35:03 -0400

    Join
    New audition tutorial video

--- join/winds/index.php
+++ join/winds/index.php
@@ -15,7 +15,7 @@
 <input type='checkbox' id='mobileMenu'>
 
 <div id="mobileHeader">
-	<a href="/"><img id="tbMobileLogo" src="/img/logos/TigerBandMobileLogo.png" width="120"></a>
+	<!--- <a href="/">---><a href="/website/"><img id="tbMobileLogo" src="/img/logos/TigerBandMobileLogo.png" width="120"></a>
 	<label for="mobileMenu" id="menuButton"><span>=</span></label>
 </div>
 
@@ -24,7 +24,8 @@
 <!--- NAVIGATION --->
 
 <nav>
-    <a href="/">Home</a>
+    <!--- <a href="/">Home</a> --->
+    <a href="/website/">Home</a>
     <a href="/this_season/">This Season</a>
     <a href="/join/" class="active">Join</a>
     <a href="/bands/">Bands</a>
@@ -47,7 +48,7 @@
             <article class="text fullWidth">
                 <h1 class="purpleText">2018 Tiger Band Brass and Woodwind Auditions</h1>
                 <p>All auditions for wind players for the 2018 Tiger Band season will be completed by video submission. Cell phone videos are acceptable if that is the only equipment available. All of the information you need to complete and submit your video, including a video tutorial is posted below. If you still have questions, please contact the band office at 864.656.3380.</p>
-                <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSfFtEfwMUih4pBOujh7BZyesDK0dUKbfnBOD9rjHv5GPrlj2w/viewform?usp=sf_link" target="_blank" class="richLink cta">Tiger Band Application</a></p>
+                <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSfFtEfwMUih4pBOujh7BZyesDK0dUKbfnBOD9rjHv5GPrlj2w/viewform?usp=sf_link" class="richLink cta">Tiger Band Application</a></p>
                 <p>The first round of drumline auditions will be via video submission due on June 1. Please see the <a href="http://tband.people.clemson.edu/drumline/">Clemson University Drumline website</a> for more details.</p>
             </article>
         </div>
@@ -81,11 +82,11 @@
                     </div>
                     <div class="deadlinesInfo" id="info1">
                         <p>Returners Video Audition Submission Deadline</p>
-                        <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSfFtEfwMUih4pBOujh7BZyesDK0dUKbfnBOD9rjHv5GPrlj2w/viewform?usp=sf_link" target="_blank" class="richLink cta">Tiger Band Application</a></p>
+                        <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSfFtEfwMUih4pBOujh7BZyesDK0dUKbfnBOD9rjHv5GPrlj2w/viewform?usp=sf_link" class="richLink cta">Tiger Band Application</a></p>
                     </div>
                     <div class="deadlinesInfo" id="info2">
                         <p>First Year Members Video Audition Submission Deadline</p>
-                        <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSfFtEfwMUih4pBOujh7BZyesDK0dUKbfnBOD9rjHv5GPrlj2w/viewform?usp=sf_link" target="_blank" class="richLink cta">Tiger Band Application</a></p>
+                        <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSfFtEfwMUih4pBOujh7BZyesDK0dUKbfnBOD9rjHv5GPrlj2w/viewform?usp=sf_link" class="richLink cta">Tiger Band Application</a></p>
                     </div>
                     <div class="deadlinesInfo" id="info3">
                         <p>Results will be emailed to everyone by July 30, 2018</p>
@@ -129,25 +130,25 @@
                 <p>Returners: Please use the posted part for your section for the audition regardless of what part you played in previous seasons of Tiger Band.</p>
             </article>
             <article class="text fullWidth">
-                <h1>Tutorial</h1>
-                <p>Here’s a video tutorial walking you through the audition and submission process:</p>
-                <div class="iframeContainer">
-                    <div class="iFrameAspectRatio">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/sXuB6ueTkcc" frameborder="0" allowfullscreen></iframe>
-                    </div>
-                </div>
-            </article>
-        </div>
-
-        <div id="col2Flex">
-            <article class="text fullWidth">
                 <h1>Submission</h1>
                 <p>Upload your video to YouTube as an “unlisted” video or to another file sharing service like Google Drive, Dropbox, or Box.</p>
                 <p>Secure a download link for the file that you can share with us.</p>
                 <p>Fill out the online application below:</p>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfFtEfwMUih4pBOujh7BZyesDK0dUKbfnBOD9rjHv5GPrlj2w/viewform?usp=sf_link" target="_blank" class="richLink cta">Tiger Band Application</a>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfFtEfwMUih4pBOujh7BZyesDK0dUKbfnBOD9rjHv5GPrlj2w/viewform?usp=sf_link" class="richLink cta">Tiger Band Application</a>
                 <p>Results will be communicated by July 30 via email. All accepted Tiger Band members will report to campus on <u>August 15</u> to begin our 2018 preseason camp.</p>
             </article>
+        </div>
+
+        <div id="col2Flex">
+          <article class="text fullWidth">
+              <h1>Tutorial</h1>
+              <p>Here’s a video tutorial walking you through the audition and submission process:</p>
+              <div class="iframeContainer">
+                  <div class="iFrameAspectRatio">
+                      <iframe width="560" height="315" src="https://www.youtube.com/embed/z2QuVMQ4B3c" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                  </div>
+              </div>
+          </article>
         </div>
     </div>
 
