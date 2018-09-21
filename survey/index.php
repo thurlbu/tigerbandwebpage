dc12f27a5488d4b309423335167a288e4d8c78e3
Nicholas Gustafson <nichgus7@gmail.com>
2018-09-21 15:36:16 -0400

    Web survey page redirect

---
+++ survey/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>A web page that points a browser to a different page after 2 seconds</title>
+<meta http-equiv="refresh" content="2; URL=https://docs.google.com/forms/d/e/1FAIpQLSf_D_fBC6Uv52ot7lCKM7ks8p6oUUFeQ17bcgvno-_y6SeWOA/viewform?usp=sf_link">
+<meta name="keywords" content="automatic redirection">
+</head>
+<body>
+If your browser doesn't automatically go there within a few seconds, 
+you may want to go to 
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSf_D_fBC6Uv52ot7lCKM7ks8p6oUUFeQ17bcgvno-_y6SeWOA/viewform?usp=sf_link">the destination</a> 
+manually.
+</body>
+</html>
