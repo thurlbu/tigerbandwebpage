962c78acdd45cc8fbf56bcb119060e6831e1119e
Adam Patyk <apatyk@g.clemson.edu>
2018-09-25 19:37:05 -0400

    Added new secure login for members only page

--- members/index.php
+++ members/index.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+
+if(isset($_POST['submit_pass']) && $_POST['pass']) {
+  $pass = $_POST['pass'];
+  if($pass == "hurly") {
+    $_SESSION['password'] = $pass;
+  } else {
+    $error="Incorrect Password";
+  }
+}
+
+if(isset($_POST['page_logout'])) {
+  unset($_SESSION['password']);
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -37,316 +53,30 @@
 <!--- PAGE CONTENT --->
 
 <body>
-
-    <!--- COVER/PASSWORD SCREEN --->
-
-    <div id="coverScreen">
-        <div class="joinFullscrContainer">
-            <div id="screenShade"></div>
-        </div>
-
-        <div class="fullpageTitle members">
-            <h2>Tiger Band</h2>
-            <h1>Members Only</h1>
-        </div>
-
-        <div id="pwdFlex">
-            <input type="password" name="password" placeholder="Password" id="password" onclick="pwdVerif()" class="" autofocus>
-        </div>
+  <?php
+    if($_SESSION['password'] == "hurly") {
+      include("./secure.php");
+    } else {
+  ?>
+  <div id="coverScreen">
+    <div class="joinFullscrContainer">
+      <div id="screenShade"></div>
     </div>
 
-    <!--- MEMBERS PAGE CONTENT --->
-
-    <div id="membersContent" class="hidden">
-        <div id="heading">
-            <h3>Members Only</h3>
-            <h4>Tiger Band</h4>
-        </div>
-
-        <div id="Col2FlexContent" class="about">
-            <div id="col1Flex">
-                <article class="text fullWidth">
-                    <h1>A Note from the Directors</h1>
-                    <p>As an ambassador of Clemson University, Tiger Band provides inspiring spirit and entertainment to the university community and beyond.</p>
-                    <p>Tiger Band is committed to outstanding, creative performances on and off the playing field through the dedication of its staff and members. It also provides its students the opportunity to develop life skills through training in teamwork and leadership.</p>
-                </article>
-            </div>
-        </div>
-
-        <div id="itineraries">
-            <div id="heading">
-                <h4>Itineraries &amp; Schedules</h4>
-            </div>
-            <div id="expandFiles1" class="fileUIcontainer">
-                <p>There's nothing here yet.</p>
-            </div>
-        </div>
-
-        <div id="excuseLetters">
-            <div id="heading">
-                <h4>Excuse Letters</h4>
-            </div>
-            <div id="expandFiles2" class="fileUIcontainer">
-                <p>There's nothing here yet.</p>
-            </div>
-        </div>
-
-        <div id="drill">
-            <div id="heading">
-                <h4>Media</h4>
-            </div>
-            <div id="expandFiles3" class="fileUIcontainer">
-                <p>There's nothing here yet.</p>
-            </div>
-        </div>
-
-    <!--- SHEET MUSIC UI --->
-
-    <div id="music">
-        <div id="heading">
-            <h4>Sheet Music</h4>
-        </div>
-
-        <!--- DROP DOWN HEADER --->
-
-        <h4 class="purpleText expand" onclick="expand(this)">Tiger Rag</h4>
-        <div class="expandButton">
-            <img src="/img/logos/dropdownArrow.svg" width="75">
-        </div>
-
-        <!--- FILE UI --->
-
-        <div class="fileUIcontainer">
-            <a href="/doc/TigerRag/TigerRag_Piccolo.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Piccolo</span>
-                </div>
-            </a>
-            <a href="/doc/TigerRag/TigerRag_Clarinet.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Clarinet</span>
-                </div>
-            </a>
-            <a href="/doc/TigerRag/TigerRag_AltoSax.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Alto Sax</span>
-                </div>
-            </a>
-            <a href="/doc/TigerRag/TigerRag_TenorSax.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Tenor Sax</span>
-                </div>
-            </a>
-            <a href="/doc/TigerRag/TigerRag_Trumpet.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Trumpet</span>
-                </div>
-            </a>
-            <a href="/doc/TigerRag/TigerRag_Mello.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Mellophone</span>
-                </div>
-            </a>
-            <a href="/doc/TigerRag/TigerRag_Trombone.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Trombone</span>
-                </div>
-            </a>
-            <a href="/doc/TigerRag/TigerRag_Baritone_BC.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Baritone (BC)</span>
-                </div>
-            </a>
-            <a href="/doc/TigerRag/TigerRag_Baritone_TC.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Baritone (TC)</span>
-                </div>
-            </a>
-            <a href="/doc/TigerRag/TigerRag_Sousa.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Sousaphone</span>
-                </div>
-            </a>
-        </div>
-
-        <!--- DROP DOWN HEADER --->
-
-        <h4 class="purpleText expand" onclick="expand(this)">Alma Mater</h4>
-        <div class="expandButton">
-            <img src="/img/logos/dropdownArrow.svg" width="75">
-        </div>
-
-        <!--- FILE UI --->
-
-        <div class="fileUIcontainer">
-            <a href="/doc/AlmaMater/AlmaMater_Piccolo.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Piccolo</span>
-                </div>
-            </a>
-            <a href="/doc/AlmaMater/AlmaMater_Clarinet.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Clarinet</span>
-                </div>
-            </a>
-            <a href="/doc/AlmaMater/AlmaMater_AltoSax.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Alto Sax</span>
-                </div>
-            </a>
-            <a href="/doc/AlmaMater/AlmaMater_TenorSax.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Tenor Sax</span>
-                </div>
-            </a>
-            <a href="/doc/AlmaMater/AlmaMater_Trumpet.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Trumpet</span>
-                </div>
-            </a>
-            <a href="/doc/AlmaMater/AlmaMater_Mello.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Mellophone</span>
-                </div>
-            </a>
-            <a href="/doc/AlmaMater/AlmaMater_Trombone.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Trombone</span>
-                </div>
-            </a>
-            <a href="/doc/AlmaMater/AlmaMater_Baritone_BC.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Baritone (BC)</span>
-                </div>
-            </a>
-            <a href="/doc/AlmaMater/AlmaMater_Baritone_TC.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Baritone (TC)</span>
-                </div>
-            </a>
-            <a href="/doc/AlmaMater/AlmaMater_Sousa.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Sousaphone</span>
-                </div>
-            </a>
-        </div>
-
-        <!--- DROP DOWN HEADER --->
-
-        <h4 class="purpleText expand" onclick="expand(this)">Orange Bowl</h4>
-        <div class="expandButton">
-            <img src="/img/logos/dropdownArrow.svg" width="75">
-        </div>
-
-        <!--- FILE UI --->
-
-        <div class="fileUIcontainer">
-            <a href="/doc/OrangeBowl/OrangeBowl_Piccolo.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Piccolo</span>
-                </div>
-            </a>
-            <a href="/doc/OrangeBowl/OrangeBowl_Clarinet.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Clarinet</span>
-                </div>
-            </a>
-            <a href="/doc/OrangeBowl/OrangeBowl_AltoSax.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Alto Sax</span>
-                </div>
-            </a>
-            <a href="/doc/OrangeBowl/OrangeBowl_TenorSax.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Tenor Sax</span>
-                </div>
-            </a>
-            <a href="/doc/OrangeBowl/OrangeBowl_Trumpet.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Trumpet</span>
-                </div>
-            </a>
-            <a href="/doc/OrangeBowl/OrangeBowl_Mello.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Mellophone</span>
-                </div>
-            </a>
-            <a href="/doc/OrangeBowl/OrangeBowl_Trombone.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Trombone</span>
-                </div>
-            </a>
-            <a href="/doc/OrangeBowl/OrangeBowl_Baritone_BC.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Baritone (BC)</span>
-                </div>
-            </a>
-            <a href="/doc/OrangeBowl/OrangeBowl_Baritone_TC.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Baritone (TC)</span>
-                </div>
-            </a>
-            <a href="/doc/OrangeBowl/OrangeBowl_Sousaphone.pdf" target="_blank">
-                <div class="fileUIfile">
-                    <span>Sousaphone</span>
-                </div>
-            </a>
-        </div>
+    <div class="fullpageTitle members">
+      <h2>Tiger Band</h2>
+      <h1>Members Only</h1>
     </div>
 
-    <!--- FOOTER --->
-
-    <?php
-        readfile("../footer.html");
-    ?>
+    <div class="pwdFlex">
+      <form method="post" action="" id="login_form">
+        <input type="password" name="pass" placeholder="Password" autofocus>
+        <input type="submit" name="submit_pass" value="" class="hide">
+      </form>
     </div>
-</body>
-
-<script> //JavaScript for Members Only Entry
-    var pwd = document.getElementById("password");
-    var cover = document.getElementById("coverScreen");
-    var content = document.getElementById("membersContent")
-
-    pwd.addEventListener("keydown", function (e) {
-        if (e.keyCode === 13) {
-            pwdVerif(e);
-        }
-    });
-    function pwdVerif(e) {
-        pwd = document.getElementById("password");
-        if (pwd.value == "hurly") {
-            cover.className = "fadeOut";
-            setTimeout(hide, 125);
-            window.scrollTo(0, 0);
-            pwd.blur();
-        }
-        else {
-            if (pwd.value != "") {
-                pwd.className += " shake";
-                setTimeout(removeShake, 700);
-            }
-        }
-    };
-    function hide() {
-        cover.className = "hidden";
-        content.className = "";
-    };
-    function removeShake() {
-        pwd.className = "";
-    };
-
-    //JavaScript for Collapsible Articles
-    function expand(n) {
-        n.classList.toggle("clicked");
-    };
-    //Copyright Date
-    var startYear = 2017;
-    var currentYear = new Date().getFullYear();
-    if (startYear == currentYear) {
-        document.getElementById("copyright").innerHTML = "COPYRIGHT &copy " + startYear;
-    }
-    else {
-        document.getElementById("copyright").innerHTML = "COPYRIGHT &copy " + startYear + " - " + currentYear;
-    }
-</script>
+  </div>
+  <?php
+  }
+  ?>
+  </body>
 </html>
