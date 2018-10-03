c6cee37cc69fb7c6472832a8a1e4fa902ec232b0
Nicholas Gustafson <nichgus7@gmail.com>
2018-10-03 09:21:31 -0400

    Adding gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('default', function (callback) {
 
 gulp.task('build', function (callback) {
   runSequence('clean:prod', 'sass', 'css',
-    ['move-html-php-to-prod', 'minify-css', 'images'],
+    ['move-html-php-to-prod', 'minify-css', 'images', 'drumline', 'cutba', 'move-docs'],
     callback
   );
 });
@@ -34,6 +34,12 @@ gulp.task('watch', ['browserSync'], function () {
 
   gulp.watch('./app/css/*.sass', ['sass']);
   gulp.watch('./app/css/*.css', ['css']);
+
+  gulp.watch('./app/drumline/css/*.sass', ['drumline-sass']);
+  gulp.watch('./app/drumline/css/*.css', ['drumline-css-app']);
+
+  gulp.watch('./app/cutba/css/*.sass', ['cutba-sass']);
+  gulp.watch('./app/cutba/css/*.css', ['cutba-css-app']);
 
   mainFiles.forEach(function (file) {
     gulp.watch(file, browserSync.reload);
@@ -75,7 +81,7 @@ gulp.task('move-html-php-to-prod', function () {
 
 gulp.task('images', function () {
   return gulp.src(['./app/img/*.+(png|jpg|jpeg|gif|svg)',
-    './app/img/**/*.+(png|jpg|jpeg|gif|svg)'])
+    './app/img/(logos|staff)/*.+(png|jpg|jpeg|gif|svg)'])
     .pipe(cache(imagemin({
       interlaced: true
     })))
@@ -95,3 +101,102 @@ gulp.task('clean:prod', function () {
 gulp.task('cache:clear', function (callback) {
   return cache.clearAll(callback);
 });
+
+gulp.task('move-docs', function () {
+  return gulp.src(['./app/doc/*', './app/doc/**/*'])
+    .pipe(gulp.dest('./prod/doc'));
+})
+
+
+// Doing drumline stuff
+
+gulp.task('drumline', function () {
+  runSequence(['drumline-move-docs', 'drumline-css-prod', 'drumline-images']);
+});
+
+gulp.task('drumline-sass', function () {
+  return gulp.src('./app/drumline/css/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/drumline/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('drumline-css-app', function () {
+  return gulp.src(['./app/css/main.css', './app/drumline/css/*.css'])
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./app/drumline/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+})
+
+gulp.task('drumline-css-prod', function () {
+  return gulp.src('./app/drumline/css/main.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('./prod/drumline/css/'));
+});
+
+gulp.task('drumline-images', function () {
+  return gulp.src(['./app/drumline/img/*.+(png|jpg|jpeg|gif|svg)',
+    './app/drumline/img/(logos|staff)/*.+(png|jpg|jpeg|gif|svg)'])
+    .pipe(cache(imagemin({
+      interlaced: true
+    })))
+    .pipe(gulp.dest('./prod/drumline/img'));
+});
+
+gulp.task('drumline-move-docs', function () {
+  return gulp.src(['./app/drumline/doc/*', './app/drumline/doc/**/*'])
+    .pipe(gulp.dest('./prod/drumline/doc'));
+});
+
+gulp.task('drumline-move-extra', function () {
+  return gulp.src(['./app/drumline/*', '!./app/drumline/*.html'])
+    .pipe(gulp.dest('./prod/drumline/'));
+});
+
+// Doing cutba stuff
+
+gulp.task('cutba', function (callback) {
+  runSequence(['cutba-move-docs', 'cutba-css-prod', 'cutba-images']);
+});
+
+gulp.task('cutba-sass', function () {
+  return gulp.src('./app/cutba/css/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/cutba/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('cutba-css-app', function () {
+  return gulp.src(['./app/css/main.css', './app/cutba/css/*.css'])
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./app/cutba/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+})
+
+gulp.task('cutba-css-prod', function () {
+  return gulp.src('./app/cutba/css/main.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('./prod/cutba/css/'));
+});
+
+gulp.task('cutba-images', function () {
+  return gulp.src(['./app/cutba/img/*.+(png|jpg|jpeg|gif|svg)',
+    './app/cutba/img/(logos|staff)/*.+(png|jpg|jpeg|gif|svg)'])
+    .pipe(cache(imagemin({
+      interlaced: true
+    })))
+    .pipe(gulp.dest('./prod/cutba/img'));
+});
+
+gulp.task('cutba-move-docs', function () {
+  return gulp.src(['./app/cutba/doc/*', './app/cutba/doc/**/*'])
+    .pipe(gulp.dest('./prod/cutba/doc'));
+})
