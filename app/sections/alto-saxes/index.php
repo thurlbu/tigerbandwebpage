a886d2bba3c2a2235950cf98018c57e2cefa8604
Nicholas Gustafson <nichgus7@gmail.com>
2019-02-24 18:26:15 -0500

    Alto sax page round 1

---
+++ app/sections/alto-saxes/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Alto Saxophone Section | Tiger Band</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name="description" content="The Band That Shakes the Southland">
+    <meta name="keywords" content="tiger, band, clemson, university, shakes, southland">
+</head>
+
+<input type='checkbox' id='mobileMenu'>
+
+<div id="mobileHeader">
+	<a href="/"><img id="tbMobileLogo" src="/img/logos/TigerBandMobileLogo.png" width="120"></a>
+	<label for="mobileMenu" id="menuButton"><span>=</span></label>
+</div>
+
+<label id='mobileScreenShade' for='mobileMenu'></label>
+
+<!--- NAVIGATION --->
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/this_season/">This Season</a>
+    <a href="/join/">Join</a>
+    <a href="/bands/">Bands</a>
+    <a href="/organizations/">Organizations</a>
+    <a href="/staff/">Staff</a>
+    <a href="/sections/" class="active">Sections</a>
+    <a href="/contact/">Contact</a>
+</nav>
+
+<body>
+  <div class="section-heading" id="alto-section-heading">
+    <h1>Alto Saxophones</h1>
+    <img src="/img/sections/altos/altos.png">
+  </div>
+
+  <div class="section-quote" id="alto-section-quote">
+    <p>The Alto Saxophone section of Tiger Band cultivates a jovial culture unmatched by any other section in Tiger Band, we had to have a special meeting because we were having too much fun. </p>
+    <p>&mdash; Grant</p>
+    <p>Less Dancing, More Dressing, Mr. Hurlburt, our assistant band director, said this to the Altos during rehearsal once.</p>
+    <p>&mdash; Sam</p>
+  </div>
+
+  <div class="heading">
+      <h4>Section Leaders</h4>
+  </div>
+
+  <div class="section-leaders-content">
+    <div class="section-leader">
+      <img class="section-leader-photo" src="/img/sections/altos/grant-howard.png">
+      <h3>Grant Howard</h3>
+      <p>Senior | Economics <br/>Anderson, SC</p>
+    </div>
+    <div class="section-leader">
+      <img class="section-leader-photo" src="/img/sections/altos/sam-regi.png">
+      <h3>Sam Regi</h3>
+      <p>Senior | Performing Arts <br/>Tyrone, GA</p>
+    </div>
+  </div>
+
+  <div class="heading section-pages">
+      <h4>Traditions</h4>
+  </div>
+
+  <div class="media-container">
+    <video autoplay loop muted>
+        <source src="https://s3.amazonaws.com/tiger-band-website/WebsiteVideo.mp4" type="video/mp4">
+    </video>
+  </div>
+
+  <div class="section-traditions">
+    <div class="tradition-tile">
+      <img oncontextmenu="return false;" src="/img/sections/altos/barbara-ann.png">
+    </div>
+    <div class="tradition-tile">
+      <div class="tradition-text">
+        <h4>Barbara Ann</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut magna tortor. Curabitur vehicula lobortis lacus, id dignissim metus interdum eu. Nam finibus lorem quis nibh pharetra, a facilisis diam mattis. Mauris sed arcu a enim vestibulum sodales. Duis feugiat nisl quam, ac lobortis erat rutrum eget. Nunc ultricies felis consectetur, venenatis risus ac, eleifend quam. Sed eros eros, pulvinar a nisl iaculis, suscipit cursus mi. Duis sodales, justo ut varius blandit, diam nisl vehicula turpis, a viverra nunc ex in enim.</p>
+      </div>
+    </div>
+
+    <div class="tradition-tile">
+      <div class="tradition-text">
+        <h4>Jorts</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut magna tortor. Curabitur vehicula lobortis lacus, id dignissim metus interdum eu. Nam finibus lorem quis nibh pharetra, a facilisis diam mattis. Mauris sed arcu a enim vestibulum sodales. Duis feugiat nisl quam, ac lobortis erat rutrum eget. Nunc ultricies felis consectetur, venenatis risus ac, eleifend quam. Sed eros eros, pulvinar a nisl iaculis, suscipit cursus mi. Duis sodales, justo ut varius blandit, diam nisl vehicula turpis, a viverra nunc ex in enim.</p>
+      </div>
+    </div>
+    <div class="tradition-tile">
+      <img src="/img/sections/altos/jorts.png">
+    </div>
+
+    <div class="tradition-tile">
+      <img src="/img/sections/altos/tiger-rag.png">
+    </div>
+    <div class="tradition-tile">
+      <div class="tradition-text">
+        <h4>Tiger Rag Tradition</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut magna tortor. Curabitur vehicula lobortis lacus, id dignissim metus interdum eu. Nam finibus lorem quis nibh pharetra, a facilisis diam mattis. Mauris sed arcu a enim vestibulum sodales. Duis feugiat nisl quam, ac lobortis erat rutrum eget. Nunc ultricies felis consectetur, venenatis risus ac, eleifend quam. Sed eros eros, pulvinar a nisl iaculis, suscipit cursus mi. Duis sodales, justo ut varius blandit, diam nisl vehicula turpis, a viverra nunc ex in enim.</p>
+      </div>
+  </div>
+
+  <div class="media-container">
+    <img src="/img/sections/altos/section-photo.png">
+  </div>
+
+  <div class="auditionButton">
+      <a href="/join/winds/" class="boxWrapper">
+          <div id="auditionToday" class="boxUIelement rect">
+              <div class="textContainer">
+                  <p>Join the culture</p>
+              </div>
+          </div>
+      </a>
+  </div>
+
+<!--- FOOTER --->
+
+<?php
+    readfile("../../footer.html");
+?>
+</body>
+
+</html>
