1f851837f47367979c8fbe6032eed8b6c0159d33
Nicholas Gustafson <nichgus7@gmail.com>
2019-03-02 14:44:32 -0500

    Trombone page

---
+++ app/sections/trombones/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Trombone Section | Tiger Band</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name="description" content="The Band That Shakes the Southland">
+    <meta name="keywords" content="tiger, band, clemson, university, shakes, southland">
+</head>
+
+<input type='checkbox' id='mobileMenu'>
+
+<div id="mobileHeader">
+	<a href="/"><img id="tbMobileLogo" src="/img/logos/TigerBandMobileLogo.png" width="120"></a>
+	<label for="mobileMenu" id="menuButton"><span>=</span></label>
+</div>
+
+<label id='mobileScreenShade' for='mobileMenu'></label>
+
+<!--- NAVIGATION --->
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/this_season/">This Season</a>
+    <a href="/join/">Join</a>
+    <a href="/bands/">Bands</a>
+    <a href="/organizations/">Organizations</a>
+    <a href="/staff/">Staff</a>
+    <a href="/sections/" class="active">Sections</a>
+    <a href="/contact/">Contact</a>
+</nav>
+
+<body>
+  <div class="section-heading">
+    <h1>Trombones</h1>
+    <img src="/img/sections/trombones/header.jpg">
+  </div>
+
+  <div class="section-quote trombone">
+    <p>Looking for the webpage of the best section in Tiger Band? You found it! We’re looking forward to another fun filled and exhilarating season here in Clemson. This season, we hope to continue our domination in our weekly battles with the trumpets, show off our ever-so-stylish bone socks, and cheer on our Tigers to another winning season. Keep scrolling to see what we’re all about.</p>
+    <p>&mdash; Georgia &amp; Trevor</p>
+  </div>
+
+  <div class="heading">
+      <h4>Section Leaders</h4>
+  </div>
+
+  <div class="section-leaders-content">
+    <div class="section-leader">
+      <img class="section-leader-photo" src="/img/sections/trombones/georgia-krendel.jpg">
+      <h3>Georgia Krendel</h3>
+      <p>Senior | Political Science<br/>Lynbrook, NY</p>
+    </div>
+    <div class="section-leader">
+      <img class="section-leader-photo" src="/img/sections/trombones/trevor-kimbrell.jpg">
+      <h3>Trevor Kimbrell</h3>
+      <p>Sophomore | Industrial Engineering<br/>Mt. Pleasant, SC</p>
+    </div>
+  </div>
+
+  <div class="heading section-pages">
+      <h4>Traditions</h4>
+  </div>
+
+  <div class="media-container">
+    <video autoplay loop muted>
+        <source src="https://s3.amazonaws.com/tiger-band-website/Baritone+Section+Hype+Video.mp4" type="video/mp4">
+    </video>
+  </div>
+
+  <div class="section-traditions">
+    <div class="tradition-tile">
+      <img src="/img/sections/trombones/trombone-trumpet-battles.jpg">
+    </div>
+    <div class="tradition-tile">
+      <div class="tradition-text">
+        <h4>Trombone Trumpet Battle</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut magna tortor. Curabitur vehicula lobortis lacus, id dignissim metus interdum eu. Nam finibus lorem quis nibh pharetra, a facilisis diam mattis. Mauris sed arcu a enim vestibulum sodales. Duis feugiat nisl quam, ac lobortis erat rutrum eget. Nunc ultricies felis consectetur, venenatis risus ac, eleifend quam. Sed eros eros, pulvinar a nisl iaculis, suscipit cursus mi. Duis sodales, justo ut varius blandit, diam nisl vehicula turpis, a viverra nunc ex in enim.</p>
+      </div>
+    </div>
+
+    <div class="tradition-tile">
+      <div class="tradition-text">
+        <h4>Bonesocks</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut magna tortor. Curabitur vehicula lobortis lacus, id dignissim metus interdum eu. Nam finibus lorem quis nibh pharetra, a facilisis diam mattis. Mauris sed arcu a enim vestibulum sodales. Duis feugiat nisl quam, ac lobortis erat rutrum eget. Nunc ultricies felis consectetur, venenatis risus ac, eleifend quam. Sed eros eros, pulvinar a nisl iaculis, suscipit cursus mi. Duis sodales, justo ut varius blandit, diam nisl vehicula turpis, a viverra nunc ex in enim.</p>
+      </div>
+    </div>
+    <div class="tradition-tile">
+      <img src="/img/sections/trombones/bonesocks.jpg">
+    </div>
+
+    <div class="tradition-tile">
+      <img src="/img/sections/trombones/pre-game-hype-speech.jpg">
+    </div>
+    <div class="tradition-tile">
+      <div class="tradition-text">
+        <h4>Pre Game Hype Speech </h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut magna tortor. Curabitur vehicula lobortis lacus, id dignissim metus interdum eu. Nam finibus lorem quis nibh pharetra, a facilisis diam mattis. Mauris sed arcu a enim vestibulum sodales. Duis feugiat nisl quam, ac lobortis erat rutrum eget. Nunc ultricies felis consectetur, venenatis risus ac, eleifend quam. Sed eros eros, pulvinar a nisl iaculis, suscipit cursus mi. Duis sodales, justo ut varius blandit, diam nisl vehicula turpis, a viverra nunc ex in enim.</p>
+      </div>
+    </div>
+
+    <div class="tradition-tile">
+      <div class="tradition-text">
+        <h4>Bonesgiving</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut magna tortor. Curabitur vehicula lobortis lacus, id dignissim metus interdum eu. Nam finibus lorem quis nibh pharetra, a facilisis diam mattis. Mauris sed arcu a enim vestibulum sodales. Duis feugiat nisl quam, ac lobortis erat rutrum eget. Nunc ultricies felis consectetur, venenatis risus ac, eleifend quam. Sed eros eros, pulvinar a nisl iaculis, suscipit cursus mi. Duis sodales, justo ut varius blandit, diam nisl vehicula turpis, a viverra nunc ex in enim.</p>
+      </div>
+    </div>
+    <div class="tradition-tile">
+      <img src="/img/sections/trombones/bonesgiving.jpg">
+    </div>
+  </div>
+
+  <div class="media-container">
+    <img src="/img/sections/trombones/section-photo.jpg">
+  </div>
+
+  <div class="auditionButton">
+      <a href="/join/winds/" class="boxWrapper">
+          <div id="auditionToday" class="boxUIelement rect trombone">
+              <div class="textContainer">
+                  <p>Be a part of the saga</p>
+              </div>
+          </div>
+      </a>
+  </div>
+
+<!--- FOOTER --->
+
+<?php
+    readfile("../../footer.html");
+?>
+</body>
+
+</html>
