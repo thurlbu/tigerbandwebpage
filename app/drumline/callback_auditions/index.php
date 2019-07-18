9f92b568723ee6a47825d9ea81d3b4730d333ae3
Curry <lcurry@greenville.k12.sc.us>
2019-07-17 22:22:20 -0400

    Update index.php
    Updated call-back audition information - LC

--- app/drumline/callback_auditions/index.php
+++ app/drumline/callback_auditions/index.php
@@ -137,8 +137,7 @@
                 <h1 class="purpleText">2019 Drumline Auditions</h1>
                 <p>Prospective CUD Members:</p>
                 <p>Clemson University Drumline auditions take place in 2 rounds. The first round consists of submitting a video audition. Students may (and are encouraged) to submit auditions on multiple instruments - snare drum, bass drum, tenor drum, and cymbals - if they would like to. The deadline for this electronic submission is July 1.</p>
-                <p>Auditioners will be notified about video audition results by July 8. Students who earn a call-back audition invitation will move to Clemson on August 10 to participate in the live audition round before Tiger Band's preseason camp begins.</p>
-
+                <p>Auditionees will be notified about video audition results by July 8. Students who earn a call-back audition invitation will move to Clemson on August 10 to participate in the live audition round before Tiger Band's preseason camp begins.</p>
                 <!-- <a href="video/" class="richLink cta">Video Audition Information</a> -->
 
                 <div class="linkWrapper">
@@ -151,10 +150,7 @@
             <article class="text fullWidth">
                 <h1 class="purpleText">Schedules</h1>
                 <p>A detailed band camp schedule will be sent to all prospective members in mid-late July. There will be additional weekend rehearsals up until the first football game.</p>
-                <div class="linkWrapper">
-                    <a href="" class="richLink" target="_blank">2019 Audition Schedule - coming soon...</a>
-                    <a href="" class="richLink" target="_blank">2019 Full Preseason Schedule - coming soon...</a>
-                </div>
+                <div class="linkWrapper"><a href="https://docs.google.com/document/d/18B16Zdbj1mQUcjkRoqaJjwECXwv1rQzG-sy277p81Y4/edit?usp=sharing" class="richLink" target="_blank" title="Fall Audition Schedule">2019 Audition Schedule</a> <a href="https://drive.google.com/file/d/0B4DWpMjnU4xALW4zcm92NDhDZTVtTVdVM0RvbTdoaXdjNzJz/view?usp=sharing" class="richLink" target="_blank" title="Full Preseason Schedule">2019 Full Preseason Schedule</a></div>
             </article>
         </div>
     </div>
@@ -168,8 +164,7 @@
     <div id="Col1FlexContent">
         <article class="text fullWidth">
             <h1>Fall Audition Process</h1>
-            <p>The video audition material will be announced soon. In the meantime, check out the <a href="/music/">music page</a> to begin working on some of the exercises which will be included on the audition.</p>
-            <p>The live audition process will consist of two parts; an ensemble evaluation and an individual evaluation. In the ensemble evaluation, each section will audition together as a line on a rotating basis trying various player combinations. During the individual evaluation, students will be asked to sight read and may be asked to play <strong>any</strong> of the <a href="/music/">music</a> found on the website.</p>
+<p>Following the video audition, the live audition process will consist of two parts; an ensemble evaluation and an individual evaluation. In the ensemble evaluation, each section will audition together as a line on a rotating basis trying various player combinations. During the individual evaluation, students will be asked to sight read and may be asked to play <strong>any</strong> of the <a href="/music/">music</a> found on the website.</p>
             <p>Results for the Snare and Quad lines will be posted first. Results for the Bass line will be posted second. The Cymbal line results will be posted third. <strong>Based on your video audition results, you will be given section(s) to prepare for that will give you the best opportunity to make the line.</strong> All returning members (except for the section leaders) are required to re-audition each year — all spots are open and no spots are guaranteed.</p>
         </article>
         <article class="text fullWidth">
@@ -178,7 +173,7 @@
         </article>
         <article class="text fullWidth">
             <h1>Audition Material</h1>
-            <p>Instrument assignments will be based on the ensemble audition (using the music found on the <a href="/music/">Music page</a>) and the individual audition (which includes sight reading). <strong>Memorization of the exercises and cadences are required for the audition</strong>; supplemental material and pregame music do not have to be memorized for the audition. In addition, <strong>a show excerpt will be posted on the Music page on July 8th</strong>. This excerpt is expected to be playable with musical expression and memorized for the purpose of emulating mid-season music distribution. Please check the website on July 8th in order to download the show excerpt and prepare it for auditions on August 10th.</p>
+            <p>Instrument assignments will be based on the ensemble audition (using the music found on the <a href="/music/">Music page</a>) and the individual audition (which includes sight reading). <strong>Memorization of the exercises and cadences are required for the audition</strong>; supplemental material and pregame music do not have to be memorized for the audition. In addition, <strong>a show excerpt will be posted on the Music page on August 3</strong>. This excerpt is expected to be playable with musical expression and memorized for the purpose of emulating mid-season music distribution. Please check the website on July 8th in order to download the show excerpt and prepare it for auditions on August 10th.</p>
             <p><u>Basses and Cymbals:</u><br/>When practicing split parts, Basses should choose one of the drums and Cymbals should choose one of the Cymbal parts described in the <a href="/doc/CymbalNotationLegend(2010.07.06).pdf" target="_blank">Notation Legend</a>. During the ensemble audition, <strong>you may be moved around</strong>, so being adaptable and having a basic knowledge of the part as a whole would be to your advantage. The cymbal line plays a variety of techniques that are described in the <a href="https://docs.google.com/document/d/1NUsNK8ojJ9HUMhJF1uHkof-qPFbd4pgOF6Sh3er-tUM/edit" target="_blank">Cymbal Technique Manual</a> including crashes, chokes, slides, hi-hats, smashes, sizzles, scrapes, and taps. Cymbals may be asked to play forte crashes and upbeat hi-hats while marking time to demonstrate sound quality and timing/feel. New cymbal players should also refer to the article, <a href="/doc/PBuyer-2001-MotivatingtheCymbalLine.pdf" target="_blank"><i>Motivating the Marching Cymbal Line</i></a> and do your best. We will teach you these techniques in further detail when you arrive at camp.</p>
         </article>
         <article class="text fullWidth">
