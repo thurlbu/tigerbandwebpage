ae44271f6756be815e98d665c8c8f568c98e7c56
Hurly <35848112+thurlbu@users.noreply.github.com>
2023-05-16 18:58:44 -0400

    CUTBA Page
    Hawai'i Trip

--- app/cutba/index.php
+++ app/cutba/index.php
@@ -44,8 +44,8 @@
     <a href="/" class="mobileNav" target="_blank">Tiger Band Website</a>
     <a href="https://secure.touchnet.net/C20569_ustores/web/classic/store_cat.jsp?STOREID=192&CATID=371&SINGLESTORE=true"
         class="mobileNav" target="_blank">CUTBA and Tiger Band Merchandise</a>
-    <a href="https://secure.touchnet.net:443/C20569_ustores/web/product_detail.jsp?PRODUCTID=3973" class="mobileNav"
-        target="_blank">Purchase Tiger Band Parent T-Shirts</a>
+    <a href="https://forms.gle/gyH4BV7DJqYEsEaj9" class="mobileNav"
+        target="_blank">Hawai'i trip interest form</a>
     <a href="https://secure.touchnet.net/C20569_ustores/web/store_cat.jsp?STOREID=192&CATID=368&SINGLESTORE=true" class="mobileNav" target="_blank">CUTBA Newsletter</a>
     <a href="https://iamatiger.clemson.edu/giving/cutba?utm_source=digital_website&utm_medium=tdf_link&utm_campaign=dar_online_giving_cutba&utm_term=giving_button_cutba&utm_content=departmental_site_giving_button" class="mobileNav" target="_blank">Donate to CUTBA</a>
     <a href="https://iamatiger.clemson.edu/giving/cutba/buy-a-brick" class="mobileNav">Buy a Brick</a>
@@ -93,11 +93,11 @@
                 </div>
             </a>
 
-            <a href="https://secure.touchnet.net:443/C20569_ustores/web/product_detail.jsp?PRODUCTID=3973" target="_blank">
+            <a href="https://forms.gle/gyH4BV7DJqYEsEaj9" target="_blank">
                 <div class="boxWrapper">
                     <div id="homepageNav2" class="boxUIelement rect purp">
                         <div class="textContainer">
-                            <p>Purchase Tiger Band Parent T-Shirts</p>
+                            <p>Hawai'i Trip Interest Form</p>
                         </div>
                     </div>
                 </div>
@@ -143,6 +143,16 @@
     </div>
     <div id="Col2FlexContent" class="homepage">
         <div id="col1Flex">
+
+        <article class="text">
+                <h1 class="purpleText">CUTBA is going to Hawai'i!</h1>
+                <img class="articleImg" src='img/USSMissouri.jpeg'>
+                <p>The Pearl Harbor Memorial Parade Committee has invited the Clemson University Alumni Band to represent the state of South Carolina at their event commemorating the 80th anniversary of World War II. CUTBA is working diligently to put together the trip in conjunction with, <a href="https://www.historicprograms.org/">Historic Programs</a>.</p>
+                <p>Please join us in honoring the service members who defended Pearl Harbor in Honolulu, Hawai’i. This invitation is open to any former member of the Clemson University Tiger Band, current members of CUTBA, friends of Tiger Band, and their friends and families. Please note that only Tiger Band Alumni will be eligible to participate in the performances.</p>
+                <p>The band will play a tribute concert at the USS Missouri at Pearl Harbor and parade down Kalakaua Avenue in the Pearl Harbor Memorial Parade along side other bands from around the country. Other highlights of this trip will include a tour of the island of Oahu which includes Diamond Head, Pali Lookout, Dole Plantation, North Shores Beaches, the world-famous Banzai Pipeline, Waimea Bay, and Sunset Beach. We will visit the USS Arizona Memorial, the Polynesian Cultural Center, and provide plenty of free time to explore the beaches, hiking trails, and culture of Hawai’i.</p>
+                <p>This is an opportunity for the trip of a lifetime. Please follow the link below to the interest form for the trip. This interest form will get you on the list for additional information. If you have previously expressed your interest, you do not need to complete the form again.</p>
+                <p><a href="https://forms.gle/gyH4BV7DJqYEsEaj9">HAWAI'I TRIP INTEREST FORM</a></p>
+            </article>
 
             <article class="text">
                 <h1 class="purpleText">CUTBA Wants YOU!</h1>
